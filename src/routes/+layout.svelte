f4b6f064e52ec232b6c1c9268253cbc45db37aa7
n3rd <godwinjdayo@gmail.com>
2023-10-22 02:25:38 +0100

    📦 NEW: initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+
+<script>
+	import '../app.postcss';
+
+	import { AppShell } from '@skeletonlabs/skeleton';
+</script>
+
+<AppShell>
+	<svelte:fragment slot="header">Header</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment>
+	<!-- (sidebarRight) -->
+	<!-- (pageHeader) -->
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<!-- (pageFooter) -->
+	<svelte:fragment slot="footer">Footer</svelte:fragment>
+</AppShell>
